2adb16686e78d5be5bcf7f1a69ba94f242c20ede
Samantha Fernandes <samanthaascl@gmail.com>
2025-02-15 16:18:30 -0300

    deu certo?

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-lllllllllllllllllllllll
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.gi
+
+
+
+
+1
+1
+1
